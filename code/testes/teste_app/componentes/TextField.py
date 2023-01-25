6f339e9b2dda5ac94429514b74fc71c32bc34f2f
Maicon Robert <maiconlk2321@gmail.com>
2023-01-25 12:54:36 -0300

    :recycle: refactor: Trocar funções por classes

--- code/testes/teste_app/componentes/TextField.py
+++ code/testes/teste_app/componentes/TextField.py
@@ -1,46 +1,61 @@
-import flet as ft
+from flet import TextField, TextAlign
 
-# class TextFieldTemplate:
-#     def __init__(self, label="Texto", value=0, text_align=ft.TextAlign.CENTER, width=175, suffix_text=""):
-#         self.label=label
-#         self.value=value
-#         self.text_align=text_align
-#         self.width=width
-#         self.suffix_text=suffix_text
+class TextFieldTemplate:
+    """
+    Classe para criar campos de texto com configurações comuns
+    
+    Atributos:
+        ATRIBUTO    TIPO        DEFINIÇÃO                               DEFAULT
+        label       (str)       Descrição do campo de texto             ("Texto")
+        value       (int)       Valor inicial do campo de texto         (0)
+        text_align  (TextAlign) Alinhamento do texto dentro do campo    (TextAlign.CENTER)
+        width       (int)       Largura do campo de texto               (175)
+        suffix_text (str)       Texto que aparece após o valor          ("")
+    """
 
-#     def new(self):
-#         label=self.label
-#         value=self.value
-#         text_align=self.text_align
-#         width=self.width
-#         suffix_text=self.suffix_text
-#         return ft.TextField(label=label, value=value, text_align=text_align, width=width, suffix_text=suffix_text)
+    def __init__(self, label="Texto", value=0, text_align=TextAlign.CENTER, width=175, suffix_text=""):
+        self.label=label
+        self.value=value
+        self.text_align=text_align
+        self.width=width
+        self.suffix_text=suffix_text
 
+    def build(self):
+        """
+        Cria um objeto TextField com as configurações especificadas no construtor
+        
+        Retorna: TextField: campo de texto criado
+        """
+        return TextField(
+            label=self.label, 
+            value=self.value,
+            text_align=self.text_align,
+            width=self.width,
+            suffix_text=self.suffix_text
+        )
 
-def text_fiel_template(label="Texto", value=0, text_align=ft.TextAlign.CENTER, width=175, suffix_text=""):
-    return ft.TextField(label=label, value=value, text_align=text_align, width=width, suffix_text=suffix_text)
-
-pressao = text_fiel_template(
+# Criação de campos de texto com configurações comuns
+pressao = TextFieldTemplate(
     label="Pressão",                # Descrição
     suffix_text="hPa"               # texto atras do valor, complemento
-    )
+    ).build()
 
-radiacao = text_fiel_template(
+radiacao = TextFieldTemplate(
     label="Radiacao",
     suffix_text="W/m²"
-    )
+    ).build()
 
-temperatura = text_fiel_template(
+temperatura = TextFieldTemplate(
     label="Temperatura",
     suffix_text="°C"
-    )
+    ).build()
 
-umidade = text_fiel_template(
+umidade = TextFieldTemplate(
     label="Umidade",
     suffix_text="%"
-    )
+    ).build()
 
-vento = text_fiel_template(
+vento = TextFieldTemplate(
     label="Vento",
     suffix_text="km/h"
-)
+    ).build()
