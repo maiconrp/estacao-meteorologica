fa1f3cfee547862828221646937df9b5e5c3d6ec
Maicon Robert <maiconlk2321@gmail.com>
2023-01-28 01:37:01 -0300

    :wrench: refactor: Corrigir caminho arquivo config

--- code/testes/teste_app/config/firebase/config.py
+++ code/testes/teste_app/config/firebase/config.py
@@ -1,8 +1,11 @@
 import pyrebase # biblioteca pra comunicação com firebase
 import json # biblioteca pra ler o arquivo json de configurações firebase
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # credenciais de comunicação
-with open(r"teste_app\config\firebase\config.json", 'r') as f:
+with open(BASE_DIR+"\config.json", 'r') as f:
     CONFIG = json.load(f)
 
 # configura comunicação com firebase
