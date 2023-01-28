c980507ad81aa993b43420b1ff0ac4fa8ecc2e19
Maicon Robert <maiconlk2321@gmail.com>
2023-01-28 01:43:40 -0300

    :pencil: refactor: Formatar código

--- code/testes/teste_app/componentes/TextField.py
+++ code/testes/teste_app/componentes/TextField.py
@@ -1,9 +1,10 @@
 from flet import TextField, TextAlign
 
+
 class TextFieldTemplate:
     """
     Classe para criar campos de texto com configurações comuns
-    
+
     Atributos:
         ATRIBUTO    TIPO        DEFINIÇÃO                               DEFAULT
         label       (str)       Descrição do campo de texto             ("Texto")
@@ -14,48 +15,51 @@ class TextFieldTemplate:
     """
 
     def __init__(self, label="Texto", value=0, text_align=TextAlign.CENTER, width=175, suffix_text=""):
-        self.label=label
-        self.value=value
-        self.text_align=text_align
-        self.width=width
-        self.suffix_text=suffix_text
+        self.label = label
+        self.value = value
+        self.text_align = text_align
+        self.width = width
+        self.suffix_text = suffix_text
 
     def build(self):
         """
         Cria um objeto TextField com as configurações especificadas no construtor
-        
+
         Retorna: TextField: campo de texto criado
         """
         return TextField(
-            label=self.label, 
+            label=self.label,
             value=self.value,
             text_align=self.text_align,
             width=self.width,
             suffix_text=self.suffix_text
         )
 
+
 # Criação de campos de texto com configurações comuns
+
+
 pressao = TextFieldTemplate(
     label="Pressão",                # Descrição
     suffix_text="hPa"               # texto atras do valor, complemento
-    ).build()
+).build()
 
 radiacao = TextFieldTemplate(
     label="Radiacao",
     suffix_text="W/m²"
-    ).build()
+).build()
 
 temperatura = TextFieldTemplate(
     label="Temperatura",
     suffix_text="°C"
-    ).build()
+).build()
 
 umidade = TextFieldTemplate(
     label="Umidade",
     suffix_text="%"
-    ).build()
+).build()
 
 vento = TextFieldTemplate(
     label="Vento",
     suffix_text="km/h"
-    ).build()
+).build()
