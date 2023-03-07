5b8a57e6293fc4e073844e216fc42a4ffde89aba
Maicon Robert <maiconlk2321@gmail.com>
2023-03-07 15:03:29 -0300

    :recycle: refactor: Importar componentes diretamente

--- code/testes/teste_app/pages/home.py
+++ code/testes/teste_app/pages/home.py
@@ -2,16 +2,34 @@ import flet as ft
 from componentes.NavigationBar import navigation_bar
 from componentes.AppBar import appbar
 from componentes.botões.ElevatedButton import HomeButton
-from componentes.Container import container_home
+from componentes.Container import *
+from componentes.Container import card_cultura
 
 # page
 # page.go
 class HomePage:
-    
     def build():
         return ft.View(
-                "/",
-                [
-                    container_home
+            "/",
+            [
+            Column(
+                controls=[
+                    card_cultura,
+                    line,
+                    relatorio,
+                    Row(
+                        controls=[
+                            Card(card_irrigacao, elevation=6),
+                            Card(card_economia, elevation=6),
+                        ],
+                        alignment="center",
+                    ),
+                    Card(card_ET, elevation=6),
                 ],
-            )
+                horizontal_alignment="center",
+            ),
+                navigation_bar,
+            ],
+            appbar=appbar,
+            horizontal_alignment="center",
+        )
