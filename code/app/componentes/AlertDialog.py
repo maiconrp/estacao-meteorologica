34001f077c04144f6edfb34660ea01fbe669222a
victorwilva <victorwilva@gmail.com>
2023-03-10 00:50:48 -0300

    :construction: feat: Add Alert dialog de info cultura

--- code/app/componentes/AlertDialog.py
+++ code/app/componentes/AlertDialog.py
@@ -1,7 +1,7 @@
 import flet as ft
+from flet import *
 
-
-class AlertDialogTemplate(ft.UserControl):
+"""class AlertDialogTemplate(ft.UserControl):
     def __init__(
         self,
         modal=False,
@@ -64,3 +64,142 @@ class AlertDialogTemplate(ft.UserControl):
             shape=self.shape,
             on_dismiss=self.on_dismiss,
         )
+"""
+
+class TextFieldTemplate:
+    def __init__(
+        self,
+        bgcolor='#E6E7EB',
+        width=50,
+        height=50,
+        border_radius=7,
+        read_only=True,
+        label='Field',
+        value='Info',
+        border=InputBorder.NONE
+
+    ):
+        self.bgcolor = bgcolor
+        self.width = width
+        self.height = height
+        self.border_radius = border_radius
+        self.read_only = read_only
+        self.label = label
+        self.value = value
+        self.border = border
+
+ #top_view = self.page.views[-1]
+
+    def build(self):
+        """
+        Cria um objeto TextField com as configurações especificadas no construtor
+
+        Retorna: TextField: campo de texto criado
+        """
+        return TextField(
+            bgcolor=self.bgcolor,
+            width=self.width,
+            height=self.height,
+            border_radius=self.border_radius,
+            read_only=self.read_only,
+            label=self.label,
+            value=self.value,
+            border=self.border
+        )
+
+tipo = TextFieldTemplate(width=300, label='Tipo', value='Milho').build()
+
+data = TextFieldTemplate(width=110, label='Data de plantio', value='25/08/2022').build()
+
+area = TextFieldTemplate(width=110, label='Área', value='3500m²').build()
+
+vazao = TextFieldTemplate(width=300, label='Vazão', value='1300 L/h').build()
+
+tempo_ant = TextFieldTemplate(width=70, label='Anterio', value='4h').build()
+
+tempo_atu = TextFieldTemplate(width=70, label='Atual', value='3h').build()
+
+economia = TextFieldTemplate(width=70, bgcolor='#00D154', label='Economia', value='27%').build()
+
+estagio = TextFieldTemplate(width=90, label='Estágio', value='Maturação').build()
+
+duracao = TextFieldTemplate(width=65, label='Duração', value='27 dias').build()
+
+kc = TextFieldTemplate(width=65, label='Kc', value='1.7').build()
+
+dlg_cultura = ft.AlertDialog(
+    content = Container(
+        bgcolor='white',
+        height=390,
+        width=270,
+        content = Column(
+            controls = [
+                Row(vertical_alignment=CrossAxisAlignment.CENTER,
+                    controls=[
+                        Container(
+                            content=Image(
+                                src=f"assets/icons/corn.png",
+                                width=170,
+                                height=170,
+                            ),
+                            margin=margin.only(right=-130, bottom=-60, top=-60),
+                        ),
+                        Text(
+                            "Milho",
+                            color="#000000",
+                            size=28,
+                            weight=FontWeight.W_800,
+                        )
+                    ]
+                ),
+                tipo,
+                Row(
+                    controls=[
+                            data,
+                            area,
+                    ]
+                ),
+                vazao,
+                Container(
+                    content=Text(
+                        'Tempo de irrigação',
+                        color="#000000",
+                        size=13,
+                        weight=FontWeight.W_600,
+                    )
+                ),
+                Row(
+                    controls = [
+                        tempo_ant,
+                        tempo_atu,
+                        economia,
+                    ]
+                ),
+                Container(
+                    content=Text(
+                        'Desenvolvimento',
+                        color="#000000",
+                        size=13,
+                        weight=FontWeight.W_600,
+                    )
+                ),
+                Row(
+                    controls = [
+                        estagio,
+                        duracao,
+                        kc,
+                    ]
+                ),
+            ]
+        ),         
+)         
+)
+
+
+def open_dlg_cultura(e):
+    ft.Page.dialog = dlg_cultura
+    dlg.open = True
+    ft.Page.update()
+
+
+    
