88dd0f4038e8e8e2b13a5ec7d9df4a5e69948a2c
victorwilva <victorwilva@gmail.com>
2023-03-27 00:21:08 -0300

    :sparkles: feat: Creat graphics

--- code/app/componentes/Graficos.py
+++ code/app/componentes/Graficos.py
@@ -5,59 +5,91 @@ import numpy as np
 from flet.plotly_chart import PlotlyChart
 from flet import UserControl
 import assets.colors
-from database import variaveis
+from database import valores_etc, dict_temp, grafico_temp, grafico_etc
+
+
 
 class GraficoBarras(UserControl):
     def build(self):
         green = [assets.colors.PRIMARY_GREEN,]*100
         cinza_claro = [assets.colors.CINZA_AZULADO,]*100
-
-        years = [v.val() for v in variaveis.each()]
-        print(years)
-
         fig = go.Figure()
-        fig.add_trace(go.Bar(x=years,
-                        y=[v.val() for v in variaveis.each()],
-                        name='Rest of world',
+
+        dias = [v.key() for v in valores_etc.each()]
+        fig.add_trace(go.Bar(x=dias,
+                        y=[v.val() for v in valores_etc.each()],
                         marker_color=green
                         ))
 
+        #fig = px.bar(df, x='index', y='value', color=green)
+
         fig.update_layout(
-            title='Temperatura',
-            xaxis_tickfont_size=14,
+            title='Evapotranspiração',
+            titlefont_size=28,
             yaxis=dict(
-                title='Valor (°C)',
-                titlefont_size=14,
+                title='mm/dia',
+                titlefont_size=22,
                 tickfont_size=14,
             ),
             xaxis=dict(
-                title='Hora',
-                titlefont_size=14,
-                tickfont_size=14,
+                title='Dia',
+                titlefont_size=22,
+                tickfont_size=18,
             ),
             legend=dict(
-                x=0,
+                x=1.0,
                 y=1.0,
                 bgcolor='rgba(255, 255, 255, 0)',
                 bordercolor='rgba(255, 255, 255, 0)'
             ),
             barmode='group',
-            bargap=0.15, # gap between bars of adjacent location coordinates.
+            bargap=0.1, # gap between bars of adjacent location coordinates.
             bargroupgap=0.1 # gap between bars of the same location coordinate.
         )
         
         return PlotlyChart(fig, expand=True)
 
-grafico_barras = GraficoBarras()
+grafico_Etc = GraficoBarras()
 
 class GraficoLinhas(UserControl):
     def build(self):
-        df = px.data.gapminder().query("country=='Canada'")
-        df = pd.DataFrame(dict(
-            variaveis.each()
-        ))
-        fig = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
+        green = [assets.colors.PRIMARY_GREEN,]*100
+        cinza_claro = [assets.colors.CINZA_AZULADO,]*100
+        
+        fig = go.Figure()
 
+        dias = [v.key() for v in dict_temp.each()]
+
+        print(dias)
+
+        fig.add_trace(go.Scatter(x=dias, 
+            y=[v.val() for v in dict_temp.each()], 
+            line=dict(color='#00D154', 
+            width=6)))
+
+        fig.update_layout(
+            title='Temperatura',
+            titlefont_size=28,
+            yaxis=dict(
+                title='Valor em °C',
+                titlefont_size=22,
+                tickfont_size=14,
+            ),
+            xaxis=dict(
+                title='Dia',
+                titlefont_size=22,
+                tickfont_size=18,
+            ),
+            legend=dict(
+                x=1.0,
+                y=1.0,
+                bgcolor='rgba(255, 255, 255, 0)',
+                bordercolor='rgba(255, 255, 255, 0)'
+            ),
+            barmode='group',
+            bargap=0.1, # gap between bars of adjacent location coordinates.
+            bargroupgap=0.1 # gap between bars of the same location coordinate.
+        )
         return PlotlyChart(fig, expand=True)
 
-grafico_linha = GraficoLinhas()
+grafico_temperatura = GraficoLinhas()
