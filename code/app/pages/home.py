be5c7be7a3c99541d119c82e8012aa07b118fc1a
victorwilva <victorwilva@gmail.com>
2023-03-28 00:03:08 -0300

    :tada: feat: Add new graphs

--- code/app/pages/home.py
+++ code/app/pages/home.py
@@ -3,7 +3,7 @@ from componentes.NavigationBar import navigation_bar
 from componentes.AppBar import appbar
 from componentes.botões.ElevatedButton import HomeButton
 from componentes.Container import *
-from componentes.Graficos import grafico_temperatura, grafico_Etc
+from componentes.Graficos import grafico_temperatura, grafico_Etc, grafico_umidade, grafico_vento, grafico_radiacao
 from componentes.Clima import temperatura, vento, umidade, pressao, radiacao
 
 
@@ -17,8 +17,6 @@ class HomePage:
                         card_cultura,
                         line,
                         relatorio,
-                        grafico_temperatura,
-                        grafico_Etc,
                         Row(
                             controls=[
                                 Card(card_irrigacao, elevation=6),
@@ -33,6 +31,12 @@ class HomePage:
                         Card(vento, elevation=8),
                         Card(pressao, elevation=8),
                         Card(radiacao, elevation=4),
+                        dashboard,
+                        grafico_Etc,
+                        grafico_temperatura,
+                        grafico_umidade,
+                        grafico_radiacao,
+                        grafico_vento,
                     ],
                     horizontal_alignment="center",
                 ),
