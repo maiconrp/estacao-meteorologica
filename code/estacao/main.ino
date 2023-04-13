b1bcf8c0d27cd6732b6d59daffeea4aa1b54be63
Maicon Robert <60708311+maiconrp@users.noreply.github.com>
2023-04-13 02:13:55 -0300

    :recycle: refactor: reduzir linhas de envio to firebase e repetições

--- code/estacao/main.ino
+++ code/estacao/main.ino
@@ -15,36 +15,28 @@ ArduinoID v2.0.3
 #include <DHT.h>  //Biblioteca para o sensor DHT22, v1.2.3
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 
 /* CONSTANTES */
 
 // Credenciais do firebase: Link e Senha
-#define FIREBASE_HOST "https://exemplo-nome-default-rtdb.firebaseio.com/"
-#define FIREBASE_AUTH "senha"
+#define FIREBASE_HOST "https://estacao-meteorologic-default-rtdb.firebaseio.com"
+#define FIREBASE_AUTH ""
 // senha em https://console.firebase.google.com/u/0/project/NOME-DO-PROJETO/settings/serviceaccounts/databasesecrets?hl=pt>
 
 // Credenciais de wifi: Nome e Senha
-#define WIFI_SSID "nome"
-#define WIFI_PASSWORD "senha"
+#define WIFI_SSID "estacao"
+#define WIFI_PASSWORD "meteorologica"
 
-// Pinos Sensores 
+// Pinos Sensores
 #define TEMT6000_PIN 34
 #define REED_SWITCH_PIN 19
 
 // Pressão Atmosférica local
 #define PATM 1013.25
 
-// Calibração Reed Switch
-#define VENTO_CALIBRACAO 20
-#define INTERRUPCAO_CALIBRACAO 10
-
-const float CALIBRACAO = VENTO_CALIBRACAO/INTERRUPCAO_CALIBRACAO;
-
 /* VARIÁVEIS */
-
-// wifi
-int delay_de_tentativas = 500;
-int tentativas = 20;
 
 // bmp
 float temperatura_bmp = 0;
@@ -60,8 +52,19 @@ float radiacao;
 long leitura;
 
 // reed switch
-volatile int contador_de_interrupcoes = 0; // Variável volátil para contar o número de interrupções
-float vento_reed = 0; // Velocidade atual do vento em m/s
+volatile int contador_de_interrupcoes = 0;  // Variável volátil para contar o número de interrupções
+float vento_reed = 0;                       // Velocidade atual do vento em m/s
+
+// --- Constantes ---
+const float pi = 3.14159265;     //Número de pi
+int period = 5000;               //Tempo de medida(miliseconds)
+int delaytime = 2000;            //Invervalo entre as amostras (miliseconds)
+int radius = 5.85;                //Raio do anemometro(mm)
+
+// --- Variáveis Globais ---
+unsigned int Sample  = 0;        //Armazena o número de amostras
+unsigned int counter = 0;        //Contador para o sensor
+unsigned int RPM = 0;            //Rotações por minuto
 
 /* OBJETOS */
 
@@ -71,7 +74,15 @@ Adafruit_BMP280 bmp;  // Inicialização do objeto Adafruit_BMP280 para comunica
 // sensor dht
 DHT dht(13, DHT22);  // Parametros: Pino para dados, Tipo do sensor (DHT11, 21 ou 22)
 
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
+String date;
+
 void conect_wifi() {
+  // wifi
+  int delay_de_tentativas = 500;
+  int tentativas = 40;
   /*
     Função para conectar o ESP32 ao WiFi.
 
@@ -143,15 +154,10 @@ void conect_wifi() {
   }
 }
 
-void handleInterrupt() {
-  contador_de_interrupcoes++;
-}
-
 void setup() {
-  Serial.begin(115200); // Inicia a comunicação serial
-  pinMode(REED_SWITCH_PIN, INPUT_PULLUP); // Define o pino como entrada com resistor pull-up
-  attachInterrupt(digitalPinToInterrupt(REED_SWITCH_PIN), handleInterrupt, RISING); // Configura a interrupção para ocorrer em uma borda de subida
-
+  Serial.begin(115200);                                                              // Inicia a comunicação serial
+  pinMode(REED_SWITCH_PIN, INPUT_PULLUP);                                            // Define o pino como entrada com resistor pull-up
+  
   analogReadResolution(10);
   analogSetPinAttenuation(TEMT6000_PIN, ADC_6db);
 
@@ -165,53 +171,63 @@ void setup() {
                     Adafruit_BMP280::SAMPLING_X16,    /* Oversampling da pressão */
                     Adafruit_BMP280::FILTER_X16,      /* Filtro */
                     Adafruit_BMP280::STANDBY_MS_500); /* Tempo de espera em standby */
-  };
+  }
 
   // Inicia comunicação com o sensor DHT22
   dht.begin();
-
   conect_wifi();
+
+  timeClient.begin();
+
+  timeClient.setTimeOffset(-10800); // UTC-3
 
   // Inicia comunicação com firebase
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   delay(5000);
 
+}
 
-};
+void timeGet(){
+  while(!timeClient.update()) {
+    timeClient.forceUpdate();
+  }
+  // The data comes with the following format:
+  // 2018-05-28T16:00:13Z
+  // We need to extract date and time
+  date = timeClient.getFormattedTime();
+  Serial.println(date);
+
+}
 
 void loop() {
+  timeGet();
   get_bmp();
   get_dht();
   get_temt();
   get_reed();
 
-  envio_bmp();
-  envio_dht();
-  envio_temt();
-  envio_reed();
+  envio_firebase();
 
   delay(2000);
-};
+}
 
 // Coleta
+
+void print(String sensor, float valor, String medida){
+  Serial.print(sensor);
+  Serial.print(valor);
+  Serial.println(medida);
+}
+
 void get_bmp() {
   temperatura_bmp = bmp.readTemperature();  // Leitura da temperatura em graus Celsius
   pressao_bmp = bmp.readPressure() / 100;   // Leitura da pressão atmosférica em hectopascals
   altitude_bmp = bmp.readAltitude(PATM);    // Cálculo da altitude baseado na pressão atmosférica padrão de 1013.25 hPa
 
-  Serial.print("[BMP] Temperatura: ");
-  Serial.print(temperatura_bmp);
-  Serial.println(" °C");
-
-  Serial.print("[BMP] Pressão Atmosférica: ");
-  Serial.print(pressao_bmp);
-  Serial.println(" hPa");
-
-  Serial.print("[BMP] Altitude: ");
-  Serial.print(altitude_bmp);
-  Serial.println(" m");
-  Serial.println();
-};
+  print("[BMP] Temperatura: ", temperatura_bmp, " °C");
+  print("[BMP] Pressão Atmosférica: ", pressao_bmp, " hPA");
+  print("[BMP] Altitude: ", altitude_bmp, " m");
+}
 
 void get_dht() {
   // Armazena a temperatura lida pelo sensor
@@ -219,118 +235,61 @@ void get_dht() {
   // Armazena a umidade lida pelo sensor
   umidade_dht = dht.readHumidity();
 
-  Serial.print("[DHT] Temperatura: ");
-  Serial.print(temperatura_dht);
-  Serial.println(" °C");
+  print("[DHT] Temperatura: ", temperatura_dht, " °C");
+  print("[DHT] Umidade: ", umidade_dht, " %");
+}
 
-  Serial.print("[DHT] Umidade: ");
-  Serial.print(umidade_dht);
-  Serial.println(" %");
-  Serial.println();
-};
-
-void get_temt(){
+void get_temt() {
   leitura = analogRead(TEMT6000_PIN);  //Lê o valor da entrada analógica
   radiacao = leitura * 0.0079;         //Converte o valor lido em W/m²
 
-  Serial.print("[TEMT] Leitura: ");
-  Serial.print(leitura);
-  Serial.println(" lux");
+  print("[TEMT] Leitura: ", leitura, " lux");
+  print("[TEMT] Radiação: ", radiacao, " W/m²");
+}
 
-  Serial.print("[TEMT] Radiação: ");
-  Serial.print(radiacao);
-  Serial.println(" W/m²");
-  Serial.println();
-};
+void get_reed() {
+  vento_reed = 0;
 
-void get_reed(){
-  detachInterrupt(digitalPinToInterrupt(REED_SWITCH_PIN)); // Desativa a interrupção para ler o contador
-  vento_reed = (contador_de_interrupcoes * CALIBRACAO); // Calcula a velocidade do vento em m/s
-  contador_de_interrupcoes = 0; // Zera o contador
-  attachInterrupt(digitalPinToInterrupt(REED_SWITCH_PIN), handleInterrupt, RISING); // Reativa a interrupção
+  counter = 0;
+  attachInterrupt(digitalPinToInterrupt(REED_SWITCH_PIN), addcount, RISING);
+  unsigned long millis();
+  long startTime = millis();
+  while (millis() < startTime + period) {}
 
-  Serial.print("[REED] Velocidade do Vento: ");
-  Serial.print(vento_reed);
-  Serial.println(" m/s");
-  Serial.println();
+  RPM = ((counter) * 60) / (period / 1000); // Calculate revolutions per minute (RPM)
+  vento_reed = ((4 * pi * radius * RPM) / 60) / 1000; //Calcula a velocidade do vento em m/s
+
+  print("[REED] Velocidade do Vento: ", vento_reed, " m/s");
 }
 
 // Envio
-void envio_bmp() {
-  Firebase.set("/Produtor/Cultura/Meteorologia/temperatura/valor_atual_bmp", temperatura_bmp);
-  // handle error
-  if (Firebase.failed()) {
-    Serial.print("[ERROR][bmp] Falha ao enviar temperatura:");
-    Serial.println(Firebase.error());
-    Serial.println();
-    return;
-  };
-
-  Firebase.set("/Produtor/Cultura/Meteorologia/pressao/valor_atual_bmp", pressao_bmp);
-  // handle error
-  if (Firebase.failed()) {
-    Serial.print("[ERROR][bmp] Falha ao enviar pressao:");
-    Serial.println(Firebase.error());
-    Serial.println();
-    return;
-  };
-
-  Firebase.set("/Produtor/Cultura/Meteorologia/altitude/valor_atual", altitude_bmp);
-  // handle error
-  if (Firebase.failed()) {
-    Serial.print("[ERROR][bmp] Falha ao enviar altitude:");
-    Serial.println(Firebase.error());
-    Serial.println();
-
-    return;
-  };
-};
-
-void envio_dht() {
+void send_to_firebase(String variable_name, float value) {
+  String path = "/Produtor/Cultura/Meteorologia/" + variable_name + "/historico/" + date;
   
-  Firebase.set("/Produtor/Cultura/Meteorologia/temperatura/valor_atual_dht", temperatura_dht);
-  // handle error
+  Firebase.set(path, value);
   if (Firebase.failed()) {
-    Serial.print("[ERROR][dht] Falha ao enviar temperatura:");
+    Serial.print("[ERROR][");
+    Serial.print("] Falha ao enviar valor:");
     Serial.println(Firebase.error());
     Serial.println();
-
     return;
-  };
+  }
+}
 
-  Firebase.set("/Produtor/Cultura/Meteorologia/umidade/valor_atual", umidade_dht);
-  // handle error
-  if (Firebase.failed()) {
-    Serial.print("[ERROR][bmp] Falha ao enviar umidade:");
-    Serial.println(Firebase.error());
-    Serial.println();
+void envio_firebase() {  
+  send_to_firebase("temperatura_bmp", temperatura_bmp);
+  send_to_firebase("pressao_bmp", pressao_bmp);
+  send_to_firebase("altitude_bmp", altitude_bmp);
 
-    return;
-  };
-};
+  send_to_firebase("temperatura_dht", temperatura_dht);
+  send_to_firebase("umidade", umidade_dht);
+  
+  send_to_firebase("radiacao", radiacao);
+  
+  send_to_firebase("vento", vento_reed);
+}
 
-void envio_temt() {
-  // Envia os dados de radiação em MJ para o Firebase;
-  Firebase.set("Produtor/Cultura/Meteorologia/radiacao/valor_atual", radiacao);  
-  // handle error
-  if (Firebase.failed()) {
-    Serial.print("[ERROR][temt] Falha ao enviar radiação:");
-    Serial.println(Firebase.error());
-    Serial.println();
-
-    return;
-  };
-};
-
-void envio_reed() {
-  // Envia os dados de radiação em MJ para o Firebase;
-  Firebase.set("Produtor/Cultura/Meteorologia/vento/valor_atual", vento_reed);  
-  // handle error
-  if (Firebase.failed()) {
-    Serial.print("[ERROR][reed] Falha ao enviar velocidade do vento: ");
-    Serial.println(Firebase.error());
-    Serial.println();
-
-    return;
-  };
-};
+//Incrementa contador
+void addcount() {
+  counter++;
+}
